962a654ade62a06bed75c20bec5d7b0b483d3bb2
Serhii Kupriienko <skupriienko@anaconda.com>
2025-08-26 11:51:40 +0300

    Remove ppc64le, add abs.yaml to gitignore, remove unneeded stuff

--- recipe/build.sh
+++ recipe/build.sh
@@ -7,7 +7,6 @@ mkdir -p ${PREFIX}/lib
 [[ -d "$PREFIX/lib/pkgconfig" ]] && sed -E -i "s|cudaroot=.+|cudaroot=$PREFIX|g" $PREFIX/lib/pkgconfig/nvjitlink*.pc
 
 [[ ${target_platform} == "linux-64" ]] && targetsDir="targets/x86_64-linux"
-[[ ${target_platform} == "linux-ppc64le" ]] && targetsDir="targets/ppc64le-linux"
 [[ ${target_platform} == "linux-aarch64" ]] && targetsDir="targets/sbsa-linux"
 
 for i in `ls`; do
